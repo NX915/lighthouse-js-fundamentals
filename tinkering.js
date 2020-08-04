819f3bfef7b5237f06e78d23c31130a36bb63f40
Sean Ni <nixiang915@hotmail.com>
2020-08-04 07:44:39 +0000

    udacity 7-5

--- tinkering.js
+++ tinkering.js
@@ -1,36 +1,41 @@
 /*
- * Programming Quiz: Bank Accounts 1 (7-3)
+ * Programming Quiz: Facebook Friends (7-5)
  */
 
 /*
  * QUIZ REQUIREMENTS
- * - Your code should have an object `savingsAccount` 
- * - Your `savingsAccount` object should have the `balance` and `interestRatePercent` property
- * - Your `savingsAccount` object should have a `printAccountSummary()` method
- * - Your `printAccountSummary()` method should return the EXACT expected message
- * - BE CAREFUL ABOUT THE PUNCTUATION, SPACES, AND EXACT WORDS TO BE PRINTED.
+ * - Your code should have an object `facebookProfile`
+ * - The `facebookProfile` object should have the `name` (string), `friends` (number), and `messages` (array of strings) property
+ * - Your `facebookProfile` object should have the `postMessage()`, `deleteMessage()`, `addFriend()` and `removeFriend()` method
+ * - Carefully implement the desired functionality of each method, as decribed above
  */
 
 
-var savingsAccount = {
-  balance: 1000,
-  interestRatePercent: 1,
-  deposit: function addMoney(amount) {
-      if (amount > 0) {
-          savingsAccount.balance += amount;
-      }
-  },
-  withdraw: function removeMoney(amount) {
-      var verifyBalance = savingsAccount.balance - amount;
-      if (amount > 0 && verifyBalance >= 0) {
-          savingsAccount.balance -= amount;
-      }
-  },
-  // your code goes here
-  printAccountSummary: function () {
-    return 'Welcome!\nYour balance is currently $' + savingsAccount.balance + ' and your interest rate is ' + savingsAccount.interestRatePercent + '%.'
-  }
-};
+// TIP - 
+// In an array, 
+// - addition at the end is done using push() method
+// - addition at a specific index is done using splice() method
+// - deletion from the beginning is done using pop() method
+// - deletion from a specific index is done using splice() method
 
-console.log(savingsAccount.withdraw(50));
-console.log(savingsAccount.printAccountSummary());
+// your code goes here
+
+var facebookProfile = {
+  name: 'name',
+  friends: 0,
+  messages: [],
+  postMessage: function (message) {
+    facebookProfile.messages.push(message);
+  },
+  deleteMessage: function (index) {
+    facebookProfile.messages.splice(index, 1);
+  },
+  addFriend: function () {
+    facebookProfile.friends ++;
+  },
+  removeFriend: function () {
+    if (facebookProfile.friends > 0){
+      facebookProfile.friends --;
+    }
+  }
+}
