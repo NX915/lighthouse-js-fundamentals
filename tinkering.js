4b30aad4fa7c35950a962db9e82aaeadac273c29
Sean Ni <nixiang915@hotmail.com>
2020-08-04 07:38:25 +0000

    udacity 7-4

--- tinkering.js
+++ tinkering.js
@@ -1,33 +1,36 @@
 /*
- * Programming Quiz: Umbrella (7-1)
- */
-/*
- * QUIZ REQUIREMENTS
- * - Your code should have a variable `umbrella`
- * - The variable `umbrella` should be an object
- * - Your `umbrella` object should have the `color` and `isOpen` property
- * - Your `umbrella` object should have an `open()` method that toggles the value of `isOpen` property
- * - Your `umbrella` object should have an `close()` method that toggles the value of `isOpen`
+ * Programming Quiz: Bank Accounts 1 (7-3)
  */
 
-var umbrella = {
-  color: "pink",
-  isOpen: true,
-  open: function() {
-      if (umbrella.isOpen === true) {
-          return "The umbrella is already opened!";
-      } else {
-          umbrella.isOpen = true;
-          return "Julia opens the umbrella!";
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have an object `savingsAccount` 
+ * - Your `savingsAccount` object should have the `balance` and `interestRatePercent` property
+ * - Your `savingsAccount` object should have a `printAccountSummary()` method
+ * - Your `printAccountSummary()` method should return the EXACT expected message
+ * - BE CAREFUL ABOUT THE PUNCTUATION, SPACES, AND EXACT WORDS TO BE PRINTED.
+ */
+
+
+var savingsAccount = {
+  balance: 1000,
+  interestRatePercent: 1,
+  deposit: function addMoney(amount) {
+      if (amount > 0) {
+          savingsAccount.balance += amount;
+      }
+  },
+  withdraw: function removeMoney(amount) {
+      var verifyBalance = savingsAccount.balance - amount;
+      if (amount > 0 && verifyBalance >= 0) {
+          savingsAccount.balance -= amount;
       }
   },
   // your code goes here
-  close: function() {
-    if (umbrella.isOpen === false) {
-      return "The umbrella is already closed!";
-    } else {
-      umbrella.isOpen = false;
-      return "Julia closes the umbrella!";
-    }
+  printAccountSummary: function () {
+    return 'Welcome!\nYour balance is currently $' + savingsAccount.balance + ' and your interest rate is ' + savingsAccount.interestRatePercent + '%.'
   }
 };
+
+console.log(savingsAccount.withdraw(50));
+console.log(savingsAccount.printAccountSummary());
