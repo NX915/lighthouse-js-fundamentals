1b2032323cdb0827de159606ca8d80910e3b34f0
Sean Ni <nixiang915@hotmail.com>
2020-08-04 04:21:09 +0000

    Functions last quiz

--- tinkering.js
+++ tinkering.js
@@ -1,35 +1,32 @@
 /*
- * Programming Quiz: Build A Triangle (5-3)
+ * Programming Quiz: Inline Functions (5-6)
  */
-
-/*
+ 
+ /*
  * QUIZ REQUIREMENTS
- * - Your code should have a `buildTriangle()` function
- * - Your `buildTriangle()` function should take one argument (or you can say parameter)
- * - Your `laugh()` function should build the triangle as describe above
+ * - Your code should have an `emotions()` function
+ * - Your code should call the `emotions()` function
+ * - Your `emotions()` function call should have an inline function expression passed as the second parameter
+ * - Your function expression should return the expected output
  */
 
 
-// creates a line of * for a given length
-function makeLine(length) {
-  var line = "";
-  for (var j = 1; j <= length; j++) {
-      line += "* ";
-  }
-  return line + "\n";
+// don't change this code
+// emotions() function definition
+function emotions(myString, myFunc) {
+  console.log("I am " + myString + ", " + myFunc(2));
 }
 
-function buildTriangle (num) {
+emotions ('happy', function(num) {
   let txt = '';
-  for (let i = 1; i <= num; i++){
-    txt += makeLine(i);
+  for (let i=0; i<num; i++){
+    txt += 'ha';
   }
+  txt += '!';
   return txt;
-}
-console.log(buildTriangle(10));
+});
 
-// your code goes here.  Make sure you call makeLine() in your own code.
-
-
-// test your code by uncommenting the following line
-//console.log(buildTriangle(10));
+// your code goes here
+// Call the emotions() function with two arguments
+// Argument 1 - "happy" string
+// Argument 2 - an inline function expression
